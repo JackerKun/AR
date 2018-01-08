006a51dbd9997ec0bdd7dbb992eb30d8733f7aa5
AR4DS <ar4ds@qq.com>
2018-01-08 18:13:38 +0800

    只识别

---
+++ Unity/VuforiaRecognized/Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneMgr : MonoBehaviour
+{
+	public Text text;
+	CameraSettings camSet;
+	public bool autoFocus = false;
+	void Start()
+	{
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		camSet = FindObjectOfType<CameraSettings>();
+		Switch(autoFocus);
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			autoFocus = !autoFocus;
+			Switch(autoFocus);
+		}
+	}
+
+	void Switch(bool focus)
+	{
+		camSet.SwitchAutofocus(focus);
+		text.text = focus ? "ON" : "OFF";
+	}
+
+
+}
